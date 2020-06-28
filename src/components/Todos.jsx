c5b6485df81b2b1e42dd4b9c95d1297eec1f7b0e
Jamil Ahmed <jamil.ahmed.ja28@gmail.com>
2020-06-28 16:29:56 +0500

    Update Todos.jsx

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useState } from 'react';
+import React, { useContext } from 'react';
 import { GlobalContext } from '../contextApi/GlobalState';
 import { Button, ListGroupItem } from 'reactstrap';
 
